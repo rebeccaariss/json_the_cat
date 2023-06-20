4c8fe6ea892931de7bdb32cb0c8ba814e84257f4
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-20 10:07:35 -0400

    Consolidate error handling code/remove status code & 'error: null' from successful request

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,9 +4,10 @@ const breedArg = process.argv[2]; // breed supplied by user command line argumen
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedArg}`;
 
 request(url, function(error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // In this case, what is returned as the body of the request is a string (not HTML)
+  if (error) {
+    console.log('statusCode:', response && response.statusCode); // Print the response status code
+    console.error('error:', error); // Print the error if one occurred
+  }
 
   const data = JSON.parse(body);
   if (data[0] === undefined) {
@@ -14,6 +15,7 @@ request(url, function(error, response, body) {
   } else {
     console.log(data); // Prints the data converted to object format (deserialized from JSON string into using JSON.parse)
   }
+
   // console.log(typeof data); // Prints "object"
   // console.log(data[0].weight); // Prints first entry in returned object
 });
