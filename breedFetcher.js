e23b73f03c932c56faf671c8b532dcad6cbd2c58
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-20 09:47:42 -0400

    Take user supplied breed argument from command line and make endpoint dynamic

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+const breedArg = process.argv[2]; // breed supplied by user command line argument
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedArg}`;
 
 request(url, function(error, response, body) {
   console.error('error:', error); // Print the error if one occurred
@@ -8,7 +9,7 @@ request(url, function(error, response, body) {
   // console.log('body:', body); // In this case, what is returned as the body of the request is a string (not HTML)
 
   const data = JSON.parse(body);
-  // console.log(data); // Prints the data converted to object format (deserialized from JSON string into using JSON.parse)
+  console.log(data); // Prints the data converted to object format (deserialized from JSON string into using JSON.parse)
   // console.log(typeof data); // Prints "object"
-  console.log(data[0].weight); // Prints first entry in returned object
+  // console.log(data[0].weight); // Prints first entry in returned object
 });
