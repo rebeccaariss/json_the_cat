13712a4d0f3c553306b56a95d5db6764abf997af
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-21 09:28:16 -0400

    Write tests for breedFetcher module

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,28 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns an error when an invalid/non-existent breed is entered, via callback', (done) => {
+    fetchBreedDescription('Panther', (err, desc) => {
+      assert.equal(err, "Sorry, we couldn't find the breed you entered!");
+
+      assert.equal(desc, null);
+
+      done();
+    });
+  });
+});
