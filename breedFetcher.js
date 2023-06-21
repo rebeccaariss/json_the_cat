446d24f400adf99c5561eeb086fdce8c925e4896
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-21 09:08:14 -0400

    Modify callback arguments as specified

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,15 +5,15 @@ const fetchBreedDescription = function(breedName, callback) {
 
   request(url, function(error, response, body) {
     if (error) {
-      callback(error); // Print the error if one occurred
+      callback(error, null); // Print the error if one occurred
     }
 
     if (body) {
       const data = JSON.parse(body);
       if (data[0] === undefined) {
-        callback("Sorry, we couldn't find the breed you entered!");
+        callback("Sorry, we couldn't find the breed you entered!", null);
       } else {
-        callback(data); // Returns the data converted to object format (deserialized from JSON string into using JSON.parse)
+        callback(null, data[0].description); // Returns the data converted to object format (deserialized from JSON string into using JSON.parse)
       }
     }
 
